56dae35fb9b8426ae6dd4b32793685e46e6d0ee7
dkormann <dominikormann@outlook.com>
2024-10-10 17:20:40 +0200

    style

--- src/server.py
+++ src/server.py
@@ -12,7 +12,16 @@ for field in dir(LLMFunctions):
     if func.__doc__: api_docs += f'<h3><|host|>/llm_api/{field}</h2><pre>{func.__doc__}</pre>'
 
 
-default_message = f'<h2>LLM API</h2><p>Available endpoints:</p>{api_docs}'
+default_message = f'''<h2>LLM API</h2><p>Available endpoints:</p>{api_docs}
+<style>
+  body {{ background-color: #123; font-family: Arial, sans-serif; color: #fff; margin: 5em;}}
+  @media (prefers-color-scheme: light) {{
+    body {{ background-color: #fff; color: #000; }}
+  }}
+  h1, h2, h3 {{color: #08f;}}
+
+</style>
+'''
 
 @app.route('/')
 def hello(): return default_message.replace('<|host|>', flask.request.host)
