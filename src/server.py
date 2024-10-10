7221a0ac3cf970e091f19513010d47b909aee6c0
dkormann <dominikormann@outlook.com>
2024-10-10 12:53:47 +0200

    serve

--- src/server.py
+++ src/server.py
@@ -6,15 +6,15 @@ default_message = 'this is the llm server.\nyou can ask a question at <a href="h
 @app.route('/')
 def hello(): return default_message.format(host=flask.request.host)
 
-from src.llm import host_llm
+from llm import host_llm
 llm = host_llm()
 
-@app.route('llm_completion', methods=['POST'])
+@app.route('/llm_completion', methods=['POST'])
 def llm_completion():
   params = flask.request.json
   return llm(**params)
 
-@app.route("llm_chat_completion", methods=['POST'])
+@app.route("/llm_chat_completion", methods=['POST'])
 def llm_chat_completion():
   params = flask.request.json
   return llm.create_chat_completion(**params)
