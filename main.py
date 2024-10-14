16135e1a972735a9d99793e9d2c6b849eb301d76
dkormann <dominikormann@outlook.com>
2024-10-14 16:35:25 +0200

    testing file handling

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #%%
-
 from llmservice.llm_api import LLM_API
 from llmservice.pdf_api import PDF_API
 from llmservice.store import Store
-
 from llmservice.server import server
+
+
 
 server(
   LLM_API,
