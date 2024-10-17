867dd65bf804fd7e38790d1d94c2058047e26e49
dkormann <dominikormann@outlook.com>
2024-10-17 17:17:14 +0200

    pef test works

--- llmservice/pdf_api.py
+++ llmservice/pdf_api.py
@@ -6,43 +6,42 @@ import numpy as np
 import flask
 
 class PDF_API:
-    """Handling PDF OCR"""
-
-    @staticmethod
-    def handle_pdf():
-        '''Extract text from a PDF, DOCX, or image file uploaded as form-data.'''
-        if 'file' not in flask.request.files:
-            return {'error': 'No file was uploaded'}, 400
-        
-        uploaded_file = flask.request.files['file']
-
-        extracted_text = PDF_API.get_text_from_document(uploaded_file)
-        return {'text': extracted_text}
-
-    @staticmethod
-    def get_text_from_document(uploaded_file):
-        raw_text = ""
-        images = []
-        reader = easyocr.Reader(['de'])  
-
-        file_type = uploaded_file.mimetype 
-
-        if file_type == 'application/pdf':
-            zoom = 4
-            pdf_doc = pymupdf.open(stream=uploaded_file.read(), filetype='pdf')  
-            for page in pdf_doc:
-                pixmap = page.get_pixmap(matrix=pymupdf.Matrix(zoom, zoom))
-                images.append(Image.open(BytesIO(pixmap.tobytes("png")))) 
-
-        elif 'image' in file_type:
-            images = [Image.open(BytesIO(uploaded_file.read()))]  
-
-        # Perform OCR on the images
-        for image in images:
-            raw_text += "\n".join([result[1] for result in reader.readtext(np.array(image))]) + "\n"
-
-        return raw_text
+  """Handling PDF OCR"""
+  @staticmethod
+  def handle_pdf():
+    '''Extract text from a PDF, DOCX, or image file uploaded as form-data.'''
+    if 'file' not in flask.request.files:
+      return {'error': 'No file was uploaded'}, 400
+    
+    uploaded_file = flask.request.files['file']
+    extracted_text = PDF_API.get_text_from_document(uploaded_file)
+    return {'text': extracted_text}
+  @staticmethod
+  def get_text_from_document(uploaded_file):
+    raw_text = ""
+    images = []
+    reader = easyocr.Reader(['de'])  
+    file_type = uploaded_file.mimetype 
+    if file_type == 'application/pdf':
+      zoom = 4
+      pdf_doc = pymupdf.open(stream=uploaded_file.read(), filetype='pdf')  
+      for page in pdf_doc:
+        pixmap = page.get_pixmap(matrix=pymupdf.Matrix(zoom, zoom))
+        images.append(Image.open(BytesIO(pixmap.tobytes("png")))) 
+    elif 'image' in file_type:
+      images = [Image.open(BytesIO(uploaded_file.read()))]  
+    # Perform OCR on the images
+    for image in images:
+      raw_text += "\n".join([result[1] for result in reader.readtext(np.array(image))]) + "\n"
+    return raw_text
 
 
+#%%
 
+from werkzeug.datastructures import FileStorage
 
+with open("./test/doc.pdf", "rb") as f:
+  file = FileStorage(f, content_type="application/pdf")
+
+  print(file.filename)
+  print(file.mimetype)
--- test/pdf_test.py
+++ test/pdf_test.py
@@ -1,13 +1,18 @@
+
+#%%
 import unittest
 from llmservice.pdf_api import PDF_API
+from werkzeug.datastructures import FileStorage
 
 doc = 'test/doc.pdf'
 doc2 = 'test/flight_acc.pdf'
 
 class TestPDFExtraction(unittest.TestCase):
   def test_extraction_from_doc(self):
-    # extracted_text = PDF_API.get_text_from_document(doc2)
-    # assert "Booking No. 35160458884" in extracted_text
-    pass
+    with open("test/doc.pdf", "rb") as f:
+      file = FileStorage(f, content_type="application/pdf")
+      read_data = PDF_API.get_text_from_document(file)
+    assert "Verdienstabrechnung" in read_data
+    assert "65200700K024" in read_data
 
 if __name__ == "__main__": unittest.main()
